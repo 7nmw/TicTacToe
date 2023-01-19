bcd3c60c7d43a255698a4922b8c567b0c0f79cc0
Dinislam Turaykhanov <116585464+7nmw@users.noreply.github.com>
2023-01-19 13:17:29 +0300

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,64 +1,66 @@
-#cran – решетка игры
-#proverka – принимает и проверяет ввод пользователя
-#pobeda – проверяет выиграл ли игрок
-#zapusk –  запускает функции
 print("""
 Крестики-нолики
 Игроки X и O вводят по очереди цифры от 1 до 9
 Начало игры
 """)
 
-cran = list(range(1, 10))
+grid = list(range(1, 10))
 
-def create_tabl(cran):
-   print("#" * 13)
-   for i in range(3):
-      print("#", cran[0+i*3], "#", cran[1+i*3], "#", cran[2+i*3], "#")
-      print("#" * 13)
 
-def proverka(chislo):
-   valid = False
-   while not valid:
-      otvet = input("Введите целое число от 1 до 9. Игрок" + chislo + "?")
-      try:
-         otvet = int(otvet)
-      except:
-         print("Error. Введите целое число от 1 до 9?")
-         continue
-      if 1 <= otvet <= 9:
-          if(str(cran[otvet-1]) not in "XO"):
-              cran[otvet-1] = chislo
-              valid = True
-          else:
-              print("Такой ход уже был. Сделайте другой ход")
-      else:
-          print("Error. Введите целое число от 1 до 9.")
+def create_tabl(grid):
+    print("#" * 13)
+    for i in range(3):
+        print("#", grid[0 + i * 3], "#", grid[1 + i * 3], "#", grid[2 + i * 3], "#")
+        print("#" * 13)
 
-def pobeda(cran):
-   pobeda_cod = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in pobeda_cod:
-       if cran[each[0]] == cran[each[1]] == cran[each[2]]:
-          return cran[each[0]]
-   return False
 
-def zapusk(cran):
+def check(number):
+    valid = False
+    while not valid:
+        answer = input("Введите целое число от 1 до 9. Игрок" + number + "?")
+        try:
+            answer = int(answer)
+        except:
+            print("Error. Введите целое число от 1 до 9?")
+            continue
+        if 1 <= answer <= 9:
+            if str(grid[answer - 1]) not in "XO":
+                grid[answer - 1] = number
+                valid = True
+            else:
+                print("Такой ход уже был. Сделайте другой ход")
+        else:
+            print("Error. Введите целое число от 1 до 9.")
+
+
+def Win(grid):
+    win_cod = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_cod:
+        if grid[each[0]] == grid[each[1]] == grid[each[2]]:
+            return grid[each[0]]
+    return False
+
+
+def run(grid):
     counter = 0
     win = False
     while not win:
-        create_tabl(cran)
+        create_tabl(grid)
         if counter % 2 == 0:
-           proverka("X")
+            check("X")
         else:
-           proverka("O")
+            check("O")
         counter += 1
         if counter > 4:
-           fin = pobeda(cran)
-           if fin:
-              print(fin, "игрок победил")
-              win = True
-              break
+            fin = Win(grid)
+            if fin:
+                print(fin, "игрок победил")
+                win = True
+                break
         if counter == 9:
             print("ничья")
             break
-    create_tabl(cran)
-zapusk(cran)
+    create_tabl(grid)
+
+
+run(grid)
